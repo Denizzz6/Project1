1d722eec10a85455baba11414dcb933a7d5976c4
deniz <deniz@DENIZBITMEZ>
2023-02-07 18:13:52 +0300

    Proje dosyası ekle.

---
+++ methods/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Add();
+            //var result = (30);
+
+            //int number1 = 20;
+            //int number2 = 50;
+            //var result2 = Add3(ref number1, number2);
+            //Console.WriteLine(result2);
+            //Console.WriteLine(number1);
+            //Console.ReadLine();
+
+
+            //int number1 = 20;
+            //int number2 = 50;
+            //var result2 = Add3(out number1, number2);
+            //Console.WriteLine(result2);
+            //Console.WriteLine(number1);
+            Console.WriteLine(multiply(5, 7));
+            Console.WriteLine(multiply(5, 7, 8));
+            Console.WriteLine(Add4(1, 8, 7, 9, 5));
+            Console.ReadLine();
+        }
+        static void Add()
+        {
+            Console.WriteLine("added");
+        }
+        static int Add2(int number1, int number2 = 20)
+        {
+            var result = number1 + number2;
+            return result;
+        }
+
+        //static int Add3(ref int number1, int number2)
+        //{
+
+        //         number1 = 30;
+        //         return number1 + number2;
+
+        //}
+
+        static int Add3(out int number1, int number2)
+        {
+
+            number1 = 30;
+            return number1 + number2;
+
+        }
+
+        static int multiply(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+
+        static int multiply(int number1, int number2, int number3)
+        {
+            return number1 * number2 * number3;
+        }
+
+        static int Add4(int number , params int [] numbers)
+            {
+            return numbers.Sum();
+            }   
+    }
+ }
---
+++ typesandvariables/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace typesandvariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("f");
+            int number1 = 20;
+            long number2 = 50;
+            short number3 = 8;
+            byte number4 = 72;
+            double number5 = 10.8;
+            decimal number6 = 10.4m;
+            bool condition = false;
+            char character = 'a';
+            var number7 = 10;
+                number7 = 'a';
+            Console.WriteLine("Number1 is {0}", number1);
+            Console.WriteLine("Number2 is {0}", number2);
+            Console.WriteLine("Number3 is {0}", number3);
+            Console.WriteLine("Number4 is {0}", number4);
+            Console.WriteLine("Number5 is {0}", number5);
+            Console.WriteLine("Number6 is {0}", number6);
+            Console.WriteLine("Character is:{0}", (int)character);
+            Console.WriteLine((int)days.wednesday);
+            Console.WriteLine("Number7 is {0}", number7);
+            Console.ReadLine();
+
+        }
+    }
+}
+
+enum days
+
+
+{
+    monday,tuesday,wednesday,thursday,friday,saturday,sunday
+
+}
